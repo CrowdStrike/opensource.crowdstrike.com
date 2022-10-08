6d41f1d9720c06c8f0aa39ee4b0e5fc79204d8cd
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-10-08 13:05:27 -0400

    Test against Chrome and Firefox

--- site/testem.js
+++ site/testem.js
@@ -1,22 +1,32 @@
 'use strict';
 
+const CI_BROWSER = process.env.CI_BROWSER || 'Chrome';
+const DEV_BROWSER = process.env.DEV_BROWSER || 'Firefox';
+
 module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
-  launch_in_ci: ['Chrome'],
-  launch_in_dev: ['Chrome'],
+
+  launch_in_ci: [CI_BROWSER],
+  launch_in_dev: [DEV_BROWSER],
   browser_start_timeout: 120,
+
   browser_args: {
+    Firefox: {
+      mode: 'ci',
+      args: ['-headless'],
+    },
     Chrome: {
       ci: [
         // --no-sandbox is needed when running Chrome inside a container
         process.env.CI ? '--no-sandbox' : null,
+
         '--headless',
         '--disable-dev-shm-usage',
         '--disable-software-rasterizer',
         '--mute-audio',
         '--remote-debugging-port=0',
-        '--window-size=1440,900',
+        '--window-size=1280,720', // 720p
       ].filter(Boolean),
     },
   },
