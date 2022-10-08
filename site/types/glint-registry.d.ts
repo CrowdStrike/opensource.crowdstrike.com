d190b49ce7e3b9acad6dc7be131fe72fe01fa0d5
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-10-08 17:00:00 -0400

    Upgrade deps, try to fix some glint usage

--- site/types/glint-registry.d.ts
+++ site/types/glint-registry.d.ts
@@ -5,9 +5,28 @@ import "@glint/environment-ember-template-imports";
 import "ember-page-title/glint";
 import "@crowdstrike/ember-oss-docs/glint";
 
+import type { GitHubInfo } from 'oss/components/git-hub-info';
+import type { HelperLike, ModifierLike } from '@glint/template';
+
+declare module '@ember/modifier' {
+  export const on: ModifierLike<{
+    Args: {
+      Positional: [eventName: string, eventHandler: (event: Event) => void];
+    }
+  }>
+}
+
 
 declare module "@glint/environment-ember-loose/registry" {
   export default interface Registry {
+    GitHubInfo: typeof GitHubInfo;
 
+    // ember-page-title does not provide its own types
+    'page-title': HelperLike<{
+      Args: {
+        Positional: [string];
+      };
+      Return: string;
+    }>
   }
 }
