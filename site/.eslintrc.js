9762b49036e0bf93b1853d5d50eab121cfa67aff
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-10-08 13:10:11 -0400

    Address lint issues

--- site/.eslintrc.js
+++ site/.eslintrc.js
@@ -10,5 +10,12 @@ module.exports = {
     ...config.overrides,
     // your modifications here
     // see: https://eslint.org/docs/user-guide/configuring/configuration-files#how-do-overrides-work
+    {
+      files: ['./tailwind.config.js'],
+      rules: {
+        // doesn't understand "exports"
+        'node/no-missing-require': 'off',
+      },
+    },
   ],
 };
