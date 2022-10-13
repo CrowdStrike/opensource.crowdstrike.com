c6664537de0c27a275cffe5a6f78a9b347affc27
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-10-13 12:50:29 -0400

    Fix some alignment issues, add theme switcher everywhere, fix image at bottom, etc

--- site/scripts/png-to-webp.sh
+++ site/scripts/png-to-webp.sh
@@ -2,6 +2,8 @@
 #
 # Usage:
 #   ./scripts/png-to-webp.sh file/path.png
+#
+# Comparison: https://developers.google.com/speed/webp/gallery2
 system_name=$(uname -s)
 input_file=$1
 output_file=$(sed "s/\.png$/\.webp/" <<< $input_file)
@@ -12,9 +14,11 @@ echo "Output: $output_file"
 # On Ubuntu/De Linux:
 #   apt install webp
 if [[ "$system_name" == "Linux" ]]; then
-  cwebp $input_file -o $output_file
+  cwebp $input_file -lossless -m 6 -q 100 -o $output_file
+elif [[ "$system_name" == "Darwin" ]]; then
+  cwebp $input_file -lossless -m 6 -q 100 -o $output_file
 else
-  echo "Platform is unsupported (currently)";
+  echo "Platform is unsupported (currently: $system_name)";
   echo "Make changes for your platform in scripts/png-to-webp.sh"
   exit 1;
 fi
